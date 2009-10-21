3d0dc69f9fe1ec82ff7b10193007f165895dc1c9
Nathan Freitas <nathan@freitas.net>
2009-10-21 23:17:45 +0000

    First check-in of Orbot: the Onion Routing Robot (aka Tor on Android)
    svn:r20811

---
+++ src/org/torproject/android/TorBinaryInstaller.java
@@ -0,0 +1,158 @@
+/* Copyright (c) 2009-2011, Nathan Freitas, The Guardian Project - http://openideals.com/guardian */
+/* See LICENSE for licensing information */
+
+package org.torproject.android;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import android.util.Log;
+
+public class TorBinaryInstaller implements TorConstants {
+
+	private final static String LOG_TAG = "Tor";
+
+	
+	public TorBinaryInstaller ()
+	{
+	}
+	
+	/*
+	 * Start the binary installation if the file doesn't exist or is forced
+	 */
+	public void start (boolean force)
+	{
+		boolean binaryExists = new File(TOR_BINARY_INSTALL_PATH).exists();
+		
+		Log.i(LOG_TAG,"Tor binary exists=" + binaryExists);
+		
+		if (!binaryExists || force)
+			installFromZip ();
+		
+	}
+	
+	/*
+	 * Extract the Tor binary from the APK file using ZIP
+	 */
+	private void installFromZip ()
+	{
+		
+		try
+		{
+			ZipFile zip = new ZipFile(APK_PATH);
+	
+			ZipEntry zipen = zip.getEntry(TOR_BINARY_ZIP_KEY);
+			streamToFile(zip.getInputStream(zipen),TOR_BINARY_INSTALL_PATH);
+			
+			zipen = zip.getEntry(TORRC_ZIP_KEY);
+			streamToFile(zip.getInputStream(zipen),TORRC_INSTALL_PATH);
+			
+			zip.close();
+	
+		}
+		catch (IOException ioe)
+		{
+			Log.i(LOG_TAG,"unable to unzip tor binary from apk",ioe);
+		
+		}
+	}
+	
+	/*
+	 * Write the inputstream contents to the file
+	 */
+    private static void streamToFile(InputStream stm, String targetFilename)
+
+    {
+
+        FileOutputStream stmOut = null;
+
+        byte[] buffer = new byte[FILE_WRITE_BUFFER_SIZE];
+
+        int bytecount;
+
+       
+        File outFile = new File(targetFilename);
+        
+        try {
+            outFile.createNewFile();
+
+        	stmOut = new FileOutputStream(outFile);
+        }
+
+        catch (java.io.IOException e)
+
+        {
+
+        	Log.i(LOG_TAG,"Error opening output file " + targetFilename,e);
+
+        	return;
+        }
+
+       
+
+        try
+
+        {
+
+            while ((bytecount = stm.read(buffer)) > 0)
+
+            {
+
+                stmOut.write(buffer, 0, bytecount);
+
+            }
+
+            stmOut.close();
+
+        }
+
+        catch (java.io.IOException e)
+
+        {
+
+            Log.i(LOG_TAG,"Error writing output file '" + targetFilename + "': " + e.toString());
+
+            return;
+
+        }
+
+    }
+	
+    //copy the file from inputstream to File output - alternative impl
+	public void copyFile (InputStream is, File outputFile)
+	{
+		
+		try {
+			outputFile.createNewFile();
+			DataOutputStream out = new DataOutputStream(new FileOutputStream(outputFile));
+			DataInputStream in = new DataInputStream(is);
+			
+			int b;
+			byte[] data = new byte[1024];
+			
+			while ((b = in.read(data)) != -1) {
+				out.write(data);
+			}
+			//
+			out.flush();
+			out.close();
+			in.close();
+			// chmod?
+			
+			
+			
+		} catch (IOException ex) {
+			Log.e(LOG_TAG, "error copying binary", ex);
+		}
+
+	}
+	
+	
+
+}
---
+++ src/org/torproject/android/TorConstants.java
@@ -0,0 +1,59 @@
+/* Copyright (c) 2009-2011, Nathan Freitas, The Guardian Project - http://openideals.com/guardian */
+/* See LICENSE for licensing information */
+
+package org.torproject.android;
+
+public interface TorConstants {
+
+	//home directory of Android application
+	public final static String TOR_HOME = "/data/data/org.torproject.android/";
+
+	//name of the tor C binary
+	public final static String TOR_BINARY_ASSET_KEY = "tor";	
+	
+	//path to install the Tor binary too
+	public final static String TOR_BINARY_INSTALL_PATH = TOR_HOME + TOR_BINARY_ASSET_KEY;
+	
+	//key of the tor binary in the Zip file
+	public final static String TOR_BINARY_ZIP_KEY = "assets/" + TOR_BINARY_ASSET_KEY;
+	
+	//torrc file name
+	public final static String TORRC_ASSET_KEY = "torrc";
+	
+	//path to install torrc to within the android app data folder
+	public final static String TORRC_INSTALL_PATH = TOR_HOME + TORRC_ASSET_KEY;
+	
+	//key of the torrc file in the Zip file
+	public final static String TORRC_ZIP_KEY = "assets/" + TORRC_ASSET_KEY;
+
+	//where to send the notices log
+	public final static String TOR_LOG_PATH = TOR_HOME + "notices.log";
+
+	//how to launch tor
+	public final static String TOR_COMMAND_LINE_ARGS = "-f " + TORRC_INSTALL_PATH;
+	
+	//various console cmds
+	public final static String SHELL_CMD_CHMOD = "/system/bin/chmod";
+	public final static String SHELL_CMD_KILLALL = "/system/bin/kill";
+	public final static String SHELL_CMD_RM = "/system/bin/rm";
+	public final static String SHELL_CMD_PS = "ps";
+	public final static String CHMOD_EXE_VALUE = "777";
+	
+	//path of the installed APK file
+	public final static String APK_PATH = "/data/app/org.torproject.android.apk";
+	
+	//path to check Tor against
+	public final static String URL_TOR_CHECK = "http://check.torproject.org";
+	
+	public final static int FILE_WRITE_BUFFER_SIZE = 2048;
+	
+	//HTTP Proxy server port
+	public final static int PORT_HTTP = 8118; //just like Privoxy!
+	
+	//Socks port client connects to, server is the Tor binary
+	public final static int PORT_SOCKS = 9050;
+	
+	//what is says!
+	public final static String IP_LOCALHOST = "127.0.0.1";
+	
+}
