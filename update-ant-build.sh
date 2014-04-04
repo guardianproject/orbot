445f63f0e29574cf98d738b753e683dc5a3f6154
Nathan Freitas <nathan@freitas.net>
2014-04-03 22:22:32 -0400

    updates ant build for new actionbarcompat

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -2,5 +2,4 @@
 
 # make sure your Android SDK tools path is set in SDK_BASE
 android update project --path . --name Orbot --subprojects
-android update project --path external/ActionBarSherlock/actionbarsherlock -t android-17
-
+android update project --path external/appcompat
