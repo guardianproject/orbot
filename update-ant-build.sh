6bce7d579d286c5d8579c0646fbd3a1bd4ca4bec
Nathan Freitas <nathan@freitas.net>
2014-04-07 12:17:23 -0400

    fix ant build script and target

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # make sure your Android SDK tools path is set in SDK_BASE
-android update project --path . --name Orbot --subprojects
-android update project --path external/appcompat
+android update project -t android-19 --path . --name Orbot --subprojects
+android update lib-project -t android-19 --path external/appcompat
+android update lib-project -t android-19 --path external/superuser-commands/RootCommands-Library/
