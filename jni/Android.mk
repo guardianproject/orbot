ff8677496e43ed5180ec25500f6e126a683db7cd
Nathan Freitas <nathan@freitas.net>
2014-11-13 13:41:53 -0500

    let's not build vpn support for now

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,3 @@
-#include ../OriginalDest/Android.mk
-include ../external/badvpn/Android.mk
-#include ../kalium-jni/jni/Android.mk
+##include ../OriginalDest/Android.mk
+##include ../external/badvpn/Android.mk
+##include ../kalium-jni/jni/Android.mk
