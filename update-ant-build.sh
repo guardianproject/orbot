f4e71293040a832aec67e06db6515d914f4c8b09
Nathan Freitas <nathan@freitas.net>
2014-11-17 16:04:08 -0500

    update to SDK 21

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -2,5 +2,5 @@
 
 # make sure your Android SDK tools path is set in SDK_BASE
 android update project -t android-21 --path . --name Orbot --subprojects
-android update lib-project -t android-19 --path external/appcompat
-android update lib-project -t android-19 --path external/superuser-commands/RootCommands-Library/
+android update lib-project -t android-21 --path external/appcompat
+android update lib-project -t android-21 --path external/superuser-commands/RootCommands-Library/
