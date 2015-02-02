753e21f36711a8a605d971e1be5b08e2032d93d1
Nathan Freitas <nathan@freitas.net>
2015-02-01 23:36:07 -0500

    update makefile for badvpn tun2socks

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,4 @@
-##include ../OriginalDest/Android.mk
-##include ../external/badvpn/Android.mk
+APP_PLATFORM := 9
+##include ../external/OriginalDest/Android.mk
+include ../external/badvpn/Android.mk
 ##include ../kalium-jni/jni/Android.mk
