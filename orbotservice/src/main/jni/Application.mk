0efbb1729e8cc36c1e8711310749714f1ca01bfc
Nathan Freitas <nathan@freitas.net>
2016-11-03 17:28:01 -0400

    update for new joint arm+x86 build release

--- orbotservice/src/main/jni/Application.mk
+++ orbotservice/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI      			:= armeabi armeabi-v7a arm64-v8a
-APP_PLATFORM 			:= android-23
+APP_ABI      			:= armeabi armeabi-v7a arm64-v8a x86
+APP_PLATFORM 			:= android-16
 APP_STL      			:= stlport_static
 NDK_TOOLCHAIN_VERSION 	:= 4.9
