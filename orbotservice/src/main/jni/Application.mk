0050bfeb950c09f897345a4d114feb03dcfef557
Nathan Freitas <nathan@freitas.net>
2016-11-03 10:38:10 -0400

    update JNI targets

--- orbotservice/src/main/jni/Application.mk
+++ orbotservice/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_ABI      			:= armeabi
-APP_PLATFORM 			:= android-16
+APP_PLATFORM 			:= android-23
 APP_STL      			:= stlport_static
-NDK_TOOLCHAIN_VERSION 	:= 4.8
+NDK_TOOLCHAIN_VERSION 	:= 4.9
