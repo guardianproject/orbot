a5744d795891d0092f3127976a50381f30746fd2
Nathan Freitas <nathan@freitas.net>
2017-01-13 22:14:39 -0500

    small chnage to only check for sys iptables once

--- orbotservice/src/main/java/org/torproject/android/service/transproxy/TorTransProxy.java
+++ orbotservice/src/main/java/org/torproject/android/service/transproxy/TorTransProxy.java
@@ -103,20 +103,21 @@ public class TorTransProxy implements TorServiceConstants {
 	
 	private String findSystemIPTables ()
 	{
-
-		//if the user wants us to use the built-in iptables, then we have to find it
-		File fileIpt = new File("/system/xbin/iptables");
-
-		if (fileIpt.exists())
-			mSysIptables = fileIpt.getAbsolutePath();
-		else
+		if (mSysIptables == null)
 		{
-
-			fileIpt = new File("/system/bin/iptables");
+			//if the user wants us to use the built-in iptables, then we have to find it
+			File fileIpt = new File("/system/xbin/iptables");
 
 			if (fileIpt.exists())
 				mSysIptables = fileIpt.getAbsolutePath();
-		}
+			else
+			{
+
+				fileIpt = new File("/system/bin/iptables");
+
+				if (fileIpt.exists())
+					mSysIptables = fileIpt.getAbsolutePath();
+			}
 
 		return mSysIptables;
 	}
