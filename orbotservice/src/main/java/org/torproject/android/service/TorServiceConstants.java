fc4b0940d972d61a8a4ecdecc421bb5bb8af277d
Nathan Freitas <nathan@freitas.net>
2017-02-28 23:36:47 -0500

    update tor binary constants

--- orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
+++ orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
@@ -134,9 +134,8 @@ public interface TorServiceConstants {
     public static final String CMD_VPN_CLEAR = "vpnclear";
     public static final String CMD_UPDATE_TRANS_PROXY = "update";
     public static final String CMD_SET_EXIT = "setexit";
-    
 
-    public static final String BINARY_TOR_VERSION = "0.2.8.9armx86-openssl1.0.2j";
+    public static final String BINARY_TOR_VERSION = "0.2.9.9-openssl1.0.2k";
     public static final String PREF_BINARY_TOR_VERSION_INSTALLED = "BINARY_TOR_VERSION_INSTALLED";
     
     //obfsproxy 
