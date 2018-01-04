2a9691c7ba027dbb4f2619b27a38e132ba2984b5
n8fr8 <nathan@freitas.net>
2018-01-03 23:39:18 -0500

    make sure the back arrow works

--- app/src/main/java/org/torproject/android/ui/onboarding/BridgeWizardActivity.java
+++ app/src/main/java/org/torproject/android/ui/onboarding/BridgeWizardActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -86,6 +87,16 @@ public class BridgeWizardActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
     private void showGetBridgePrompt (final String type)
