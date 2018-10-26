293afda06d105a23cee834fa1cdc7a7d71b56700
Unknown <albert.khoury96@gmail.com>
2018-10-26 19:35:31 -0400

    added TOR Config variables

--- app/src/main/java/org/torproject/android/ParseJSONData.java
+++ app/src/main/java/org/torproject/android/ParseJSONData.java
@@ -2,8 +2,44 @@ package org.torproject.android;
 
 import org.json.*;
 
+import java.util.ArrayList;
+
 public class ParseJSONData {
-    public void parseDirAuhtorities1(){
+
+
+    public class DirAuthority  {
+        public String nickname;
+        public int orport;
+        public String v3ident;
+        public String flags;
+        public int port;
+        public String fingerprint;
+        JSONObject DirAuthority = parseDirAuhtorities1();
+        JSONObject DirAuthority2 = parseDirAuthorities2();
+    }
+
+    public class ClientTransportPlugin {
+        public String transport;
+        public String binary;
+        public String options;
+        JSONObject ClientTransportPlugin = parseClientTransportPlugin();
+    }
+
+    public class Bridge {
+        public String transport;
+        public String ip;
+        public int orport;
+        JSONObject Bridge = parseBridge();
+    }
+
+    public ArrayList<DirAuthority> dirAuthorities;
+    public JSONObject socksPort = parseSocksPort();
+    public JSONObject useBridges = parseUseBridges();
+    public JSONObject clientTransportPlugin =parseClientTransportPlugin();
+    public Bridge bridge;
+
+
+    public JSONObject parseDirAuhtorities1() {
         JSONObject DirAuthObj1 = new JSONObject();
         try {
             String nickname = DirAuthObj1.getJSONObject("DirAuthorities").getString("nickname");
@@ -16,10 +52,10 @@ public class ParseJSONData {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
+        return DirAuthObj1;
     }
 
-    public void parseDirAuthorities2(){
+    public JSONObject parseDirAuthorities2() {
         JSONObject DirAuthObj2 = new JSONObject();
         try {
             String nickname = DirAuthObj2.getJSONObject("DirAuthorities").getString("nickname");
@@ -29,53 +65,57 @@ public class ParseJSONData {
             String ip = DirAuthObj2.getJSONObject("DirAuthorities").getString("ip");
             int port = DirAuthObj2.getJSONObject("DirAuthorities").getInt("port");
             String fingerprint = DirAuthObj2.getJSONObject("DirAuthorities").getString("fingerprint");
-        }
-        catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+        return DirAuthObj2;
     }
 
-    public void parseSocksPort(){
+    public JSONObject parseSocksPort() {
         JSONObject SocksPortObj = new JSONObject();
-        try{
-            String SocksPort  = SocksPortObj.getJSONObject("SocksPort").getString("SocksPort");
-        }
-        catch (JSONException e){
+        try {
+            String SocksPort = SocksPortObj.getJSONObject("SocksPort").getString("SocksPort");
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+        return SocksPortObj;
     }
 
-    public void parseUseBridges(){
+    public JSONObject parseUseBridges() {
         JSONObject UseBridgesObj = new JSONObject();
         try {
-            int UseBridges  = UseBridgesObj.getJSONObject("UseBridges").getInt("UseBridges");
+            int UseBridges = UseBridgesObj.getJSONObject("UseBridges").getInt("UseBridges");
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return UseBridgesObj;
     }
 
-    public void parseClientTransportPlugin(){
+    public JSONObject parseClientTransportPlugin() {
         JSONObject ClientTransportObj = new JSONObject();
-        try{
-            String transport  = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("transport");
-            String binary  = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("binary");
-            String options  = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("options");
-        }
-        catch (JSONException e){
+        try {
+            String transport = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("transport");
+            String binary = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("binary");
+            String options = ClientTransportObj.getJSONObject("ClientTransportPlugin").getString("options");
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+        return ClientTransportObj;
     }
 
-    public void parseBridge(){
+    public JSONObject parseBridge() {
         JSONObject BridgeObj = new JSONObject();
         try {
-            String transport  = BridgeObj.getJSONObject("Bridge").getString("transport");
-            String ip  = BridgeObj.getJSONObject("Bridge").getString("ip");
-            int orport  = BridgeObj.getJSONObject("Bridge").getInt("orport");
+            String transport = BridgeObj.getJSONObject("Bridge").getString("transport");
+            String ip = BridgeObj.getJSONObject("Bridge").getString("ip");
+            int orport = BridgeObj.getJSONObject("Bridge").getInt("orport");
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return BridgeObj;
     }
 }
+
+
 
 
