351ef96f0794bcfa38ceb7bf955c3e3dff966218
bim <dsnake@protonmail.com>
2019-06-26 17:40:24 -0400

    Removes references to Orfox since it is being completely replaced with the Tor Browser for Android. Conversations surrounding this can be found here https://blog.torproject.org/comment/281767#comment-281767 and here https://trac.torproject.org/projects/tor/ticket/29955
    Previously the Orfox link was broken if the user didn't have Google Play
    installed since it was no longer distributed on F-Droid.

--- orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
+++ orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
@@ -8,8 +8,7 @@ import android.content.Intent;
 public interface TorServiceConstants {
 
 	String TOR_APP_USERNAME = "org.torproject.android";
-	String BROWSER_APP_USERNAME = "info.guardianproject.orfox";
-	
+
 	//String DIRECTORY_TOR_BINARY = "bin";
 	String DIRECTORY_TOR_DATA = "data";
 
