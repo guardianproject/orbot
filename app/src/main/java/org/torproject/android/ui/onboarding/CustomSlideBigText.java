ee3e05423abd4f68b146711b21b1053f5742c669
Hashik Donthineni <hashikdonthineni@gmail.com>
2020-02-27 21:45:04 +0530

    Fixed blank screen when orintation changed in AppIntro

--- app/src/main/java/org/torproject/android/ui/onboarding/CustomSlideBigText.java
+++ app/src/main/java/org/torproject/android/ui/onboarding/CustomSlideBigText.java
@@ -1,14 +1,17 @@
 package org.torproject.android.ui.onboarding;
 
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
 import org.torproject.android.R;
 
 public class CustomSlideBigText extends Fragment {
@@ -19,6 +22,8 @@ public class CustomSlideBigText extends Fragment {
     private String mButtonText;
     private String mSubTitle;
     private View.OnClickListener mButtonListener;
+    TextView tv, title;
+    Button button;
 
     public static CustomSlideBigText newInstance(int layoutResId) {
         CustomSlideBigText sampleSlide = new CustomSlideBigText();
@@ -30,15 +35,15 @@ public class CustomSlideBigText extends Fragment {
         return sampleSlide;
     }
 
-    public void setTitle (String title)
-    {
+    public void setTitle(String title) {
         mTitle = title;
     }
 
-    public void setSubTitle(String subTitle) { mSubTitle = subTitle; }
+    public void setSubTitle(String subTitle) {
+        mSubTitle = subTitle;
+    }
 
-    public void showButton (String buttonText, View.OnClickListener buttonListener)
-    {
+    public void showButton(String buttonText, View.OnClickListener buttonListener) {
         mButtonText = buttonText;
         mButtonListener = buttonListener;
     }
@@ -57,19 +62,17 @@ public class CustomSlideBigText extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(layoutResId, container, false);
-        ((TextView)view.findViewById(R.id.custom_slide_big_text)).setText(mTitle);
-
+        title = ((TextView) view.findViewById(R.id.custom_slide_big_text));
+        title.setText(mTitle);
+        tv = (TextView) view.findViewById(R.id.custom_slide_big_text_sub);
         if (!TextUtils.isEmpty(mSubTitle)) {
 
-            TextView tv =
-                    (TextView)view.findViewById(R.id.custom_slide_big_text_sub);
             tv.setText(mSubTitle);
             tv.setVisibility(View.VISIBLE);
         }
 
-        if (mButtonText != null)
-        {
-            Button button = (Button)view.findViewById(R.id.custom_slide_button);
+        if (mButtonText != null) {
+            button = (Button) view.findViewById(R.id.custom_slide_button);
             button.setVisibility(View.VISIBLE);
             button.setText(mButtonText);
             button.setOnClickListener(mButtonListener);
@@ -77,4 +80,28 @@ public class CustomSlideBigText extends Fragment {
         return view;
 
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            savedInstanceState.getString("Title");
+            tv.setText(savedInstanceState.getString("SubTitle"));
+            if (mButtonText != null) {
+                button.setText(savedInstanceState.getString("ButtonText"));
+            }
+
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("Title", mTitle);
+        outState.putString("SubTitle", mSubTitle);
+        if (mButtonText != null) {
+            outState.putString("ButtonText", mButtonText);
+        }
+    }
+
 }
