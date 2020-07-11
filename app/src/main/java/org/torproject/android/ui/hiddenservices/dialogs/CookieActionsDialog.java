88082af9cf5f28c7f2398a1ff2f2cda3b63c77d4
bim <dsnake@protonmail.com>
2020-07-11 15:20:04 -0400

    Fixes #360 "Click again for backup"

--- app/src/main/java/org/torproject/android/ui/hiddenservices/dialogs/CookieActionsDialog.java
+++ app/src/main/java/org/torproject/android/ui/hiddenservices/dialogs/CookieActionsDialog.java
@@ -1,6 +1,5 @@
 package org.torproject.android.ui.hiddenservices.dialogs;
 
-
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.appcompat.app.AlertDialog;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 import org.torproject.android.R;
 import org.torproject.android.ui.hiddenservices.backup.BackupUtils;
@@ -18,6 +16,7 @@ import org.torproject.android.ui.hiddenservices.permissions.PermissionManager;
 
 public class CookieActionsDialog extends DialogFragment {
     public static final int WRITE_EXTERNAL_STORAGE_FROM_COOKIE_ACTION_DIALOG = 4;
+    private AlertDialog actionDialog;
 
     @NonNull
     @Override
@@ -25,55 +24,18 @@ public class CookieActionsDialog extends DialogFragment {
         final Bundle arguments = getArguments();
 
         final View dialog_view = getActivity().getLayoutInflater().inflate(R.layout.layout_cookie_actions, null);
-        final AlertDialog actionDialog = new AlertDialog.Builder(getActivity())
+        actionDialog = new AlertDialog.Builder(getActivity())
                 .setView(dialog_view)
                 .setTitle(R.string.client_cookies)
                 .create();
 
-        Button backup = (Button) dialog_view.findViewById(R.id.btn_cookie_backup);
-        backup.setOnClickListener(new View.OnClickListener() {
+        dialog_view.findViewById(R.id.btn_cookie_backup).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Context mContext = v.getContext();
-
-                if (PermissionManager.isLollipopOrHigher()
-                        && !PermissionManager.hasExternalWritePermission(mContext)) {
-
-                    PermissionManager.requestExternalWritePermissions(
-                            getActivity(), WRITE_EXTERNAL_STORAGE_FROM_COOKIE_ACTION_DIALOG);
-
-                    return;
-                }
-
-                BackupUtils backup_utils = new BackupUtils(mContext);
-                String backupPath = backup_utils.createCookieBackup(
-                        arguments.getString("domain"),
-                        arguments.getString("auth_cookie_value"),
-                        arguments.getInt("enabled")
-                );
-
-                if (backupPath == null || backupPath.length() < 1) {
-                    Toast.makeText(mContext, R.string.error, Toast.LENGTH_LONG).show();
-                    actionDialog.dismiss();
-                    return;
-                }
-
-                Toast.makeText(mContext, R.string.backup_saved_at_external_storage, Toast.LENGTH_LONG).show();
-
-                Uri selectedUri = Uri.parse(backupPath.substring(0, backupPath.lastIndexOf("/")));
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(selectedUri, "resource/folder");
-
-                if (intent.resolveActivityInfo(mContext.getPackageManager(), 0) != null) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(mContext, R.string.filemanager_not_available, Toast.LENGTH_LONG).show();
-                }
-                actionDialog.dismiss();
+                doBackup();
             }
         });
 
-        Button delete = (Button) dialog_view.findViewById(R.id.btn_cookie_delete);
-        delete.setOnClickListener(new View.OnClickListener() {
+        dialog_view.findViewById(R.id.btn_cookie_delete).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 CookieDeleteDialog dialog = new CookieDeleteDialog();
                 dialog.setArguments(arguments);
@@ -82,8 +44,7 @@ public class CookieActionsDialog extends DialogFragment {
             }
         });
 
-        Button cancel = (Button) dialog_view.findViewById(R.id.btn_cookie_cancel);
-        cancel.setOnClickListener(new View.OnClickListener() {
+        dialog_view.findViewById(R.id.btn_cookie_cancel).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 actionDialog.dismiss();
             }
@@ -91,4 +52,50 @@ public class CookieActionsDialog extends DialogFragment {
 
         return actionDialog;
     }
+
+    public void doBackup() {
+        Context mContext = getContext();
+        Bundle arguments = getArguments();
+
+        if (PermissionManager.isLollipopOrHigher()
+                && !PermissionManager.hasExternalWritePermission(mContext)) {
+
+            PermissionManager.requestExternalWritePermissions(
+                    getActivity(), WRITE_EXTERNAL_STORAGE_FROM_COOKIE_ACTION_DIALOG);
+
+            return;
+        }
+
+        BackupUtils backup_utils = new BackupUtils(mContext);
+        String backupPath;
+        try {
+            backupPath = backup_utils.createCookieBackup(
+                    arguments.getString("domain"),
+                    arguments.getString("auth_cookie_value"),
+                    arguments.getInt("enabled")
+            );
+        } catch (NullPointerException npe) {
+            backupPath = null;
+        }
+
+        if (backupPath == null || backupPath.length() < 1) {
+            Toast.makeText(mContext, R.string.error, Toast.LENGTH_LONG).show();
+            actionDialog.dismiss();
+            return;
+        }
+
+        Toast.makeText(mContext, R.string.backup_saved_at_external_storage, Toast.LENGTH_LONG).show();
+
+        Uri selectedUri = Uri.parse(backupPath.substring(0, backupPath.lastIndexOf("/")));
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(selectedUri, "resource/folder");
+
+        if (intent.resolveActivityInfo(mContext.getPackageManager(), 0) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(mContext, R.string.filemanager_not_available, Toast.LENGTH_LONG).show();
+        }
+        actionDialog.dismiss();
+    }
+
 }
