dbaf06be4533e301b0c91d553ec4203e23c2e057
Tad <tad@spotco.us>
2021-07-12 15:58:07 -0400

    Fixup 93bf7b24

--- orbotservice/src/main/java/org/torproject/android/service/vpn/OrbotVpnManager.java
+++ orbotservice/src/main/java/org/torproject/android/service/vpn/OrbotVpnManager.java
@@ -363,7 +363,7 @@ public class OrbotVpnManager implements Handler.Callback {
     private void startDNS(String pdnsPath, String torDnsHost, int torDnsPort, String pdnsdHost, int pdnsdPort) throws IOException, TimeoutException {
         String debugEnabledCmd = "";
         String debugEnabledConf = "off";
-        if (prefs.getBoolean(PREF_ENABLE_LOGGING, false)) {
+        if (Prefs.useDebugLogging()) {
             debugEnabledCmd = "-g";
             debugEnabledConf = "on";
         }
