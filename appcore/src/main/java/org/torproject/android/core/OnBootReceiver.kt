9044a9d097a361e3d2f8ebc04baaf167c2e4eabb
n8fr8 <nathan@freitas.net>
2022-03-01 14:17:59 -0500

    only start OrbotService if network connection is available - will receive multiple calls, from BOOT_COMPLETED to CONNECTIVITY_CHANGE

--- appcore/src/main/java/org/torproject/android/core/OnBootReceiver.kt
+++ appcore/src/main/java/org/torproject/android/core/OnBootReceiver.kt
@@ -3,16 +3,21 @@ package org.torproject.android.core
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
 import org.torproject.android.service.OrbotConstants
 import org.torproject.android.service.OrbotService
 import org.torproject.android.service.util.Prefs
 
+
 class OnBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Prefs.startOnBoot() && !sReceivedBoot) {
-            startService(OrbotConstants.ACTION_START_ON_BOOT, context)
-            sReceivedBoot = true
+            if (isNetworkAvailable(context)) {
+                startService(OrbotConstants.ACTION_START, context)
+                sReceivedBoot = true
+            }
         }
     }
 
@@ -27,5 +32,32 @@ class OnBootReceiver : BroadcastReceiver() {
 
     companion object {
         private var sReceivedBoot = false
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
     }
 }
