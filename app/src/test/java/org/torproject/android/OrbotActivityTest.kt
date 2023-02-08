03db823df4f4fb116cbb24e0322b349fb835f1a9
John Hess <johnhess@johns-mbp.lan>
2023-02-08 16:55:28 -0600

    move unit tests to appropriate spot

--- app/src/test/java/org/torproject/android/OrbotActivityTest.kt
+++ app/src/test/java/org/torproject/android/OrbotActivityTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 // A test that succeeds.  This is just a template for future testing and lets us test that
 // automated builds are properly running tests.
-internal class TrivialUnitTest {
+internal class OrbotActivityTest {
     @Test
     @Throws(Exception::class)
     fun testThatSucceeds() {
