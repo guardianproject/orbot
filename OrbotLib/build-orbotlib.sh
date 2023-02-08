197af55eae9b6ff04a75605ef208a898b29536c0
a <dsnake@protonmail.com>
2023-02-08 12:25:40 -0500

    update submod in script

--- OrbotLib/build-orbotlib.sh
+++ OrbotLib/build-orbotlib.sh
@@ -4,6 +4,7 @@ rm *aar *jar
 git clone https://github.com/bitmold/OrbotIPtProxy
 cd OrbotIPtProxy
 git pull
+git submodule update --init
 bash build-orbot.sh
 mv OrbotLib.aar ..
 mv OrbotLib-sources.jar ..
