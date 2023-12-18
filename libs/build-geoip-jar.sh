6712013219bbc14199679f56143aabe63b6afb01
a <dsnake@protonmail.com>
2023-12-18 16:01:26 -0500

    This is actually the latest geoip used by the tor project, previously the script in orbot just pulled from tor-android...
    commit ad22b60ff18e50e60ff96f5f96cc87957a46e7f5
    Author: Tor CI Release <no-email@torproject.org>
    Date:   Fri Dec 8 18:28:23 2023 +0000
    
        Update geoip files to match ipfire location db, 2023/12/08.

--- libs/build-geoip-jar.sh
+++ libs/build-geoip-jar.sh
@@ -1,7 +1,8 @@
-# assumes tor-android is in a directory next to orbot 
+# assumes tor is in a directory next to orbot 
 # make sure tor-android is up to date, particularly external/tor...
+rm geoip.jar
 mkdir assets
-cp -a ../../tor-android/external/tor/src/config/geoip assets/
-cp -a ../../tor-android/external/tor/src/config/geoip6 assets/
+cp -a ../../tor/src/config/geoip assets/
+cp -a ../../tor/src/config/geoip6 assets/
 zip -o geoip.jar assets/geoip assets/geoip6
 rm -rf assets
