8098d0732d7de6d1795e4cf99946f312018395a9
a <dsnake@protonmail.com>
2024-01-23 18:12:55 -0500

    Bump IPtProxy to Version 3.4.0
    This means:
    lyrebird is now at v0.1.0
    snowflake is now at v2.8.1

--- OrbotLib/build-orbotlib.sh
+++ OrbotLib/build-orbotlib.sh
@@ -2,12 +2,10 @@
 
 rm *aar *jar -v
 if [ ! -d OrbotIPtProxy ]; then
-   git clone https://github.com/guardianproject/OrbotIPtProxy
+   git clone https://github.com/bitmold/OrbotIPtProxy
 fi
 cd OrbotIPtProxy
 git fetch
 git rebase
 bash build-orbot.sh
-mv OrbotLib.aar .. -v
-mv OrbotLib-sources.jar .. -v
 cd ..
