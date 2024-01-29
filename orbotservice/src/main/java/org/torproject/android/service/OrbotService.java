34681df580117e6f05cd5547796c005490349c93
a <dsnake@protonmail.com>
2024-01-28 21:38:21 -0500

    Added debug log per syphyr's suggestion

--- orbotservice/src/main/java/org/torproject/android/service/OrbotService.java
+++ orbotservice/src/main/java/org/torproject/android/service/OrbotService.java
@@ -466,6 +466,7 @@ public class OrbotService extends VpnService implements OrbotConstants {
                 // only write out geoip files if there's an app update or they don't exist
                 if (!hasGeoip || !hasGeoip6 || Prefs.isGeoIpReinstallNeeded()) {
                     try {
+                        Log.d(TAG, "Installing geoip files...");
                         new CustomTorResourceInstaller(this, appBinHome).installGeoIP();
                         Prefs.setIsGeoIpReinstallNeeded(false);
                     } catch (IOException io) { // user has < 10MB free space on disk...
