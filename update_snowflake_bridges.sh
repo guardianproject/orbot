6586608414f0497908b3a47cfa460bc4d3eec128
a <dsnake@protonmail.com>
2024-05-25 01:32:14 -0400

    add -f to rm in script

--- update_snowflake_bridges.sh
+++ update_snowflake_bridges.sh
@@ -1,6 +1,6 @@
 wget https://gitlab.torproject.org/tpo/applications/tor-browser-build/-/raw/main/projects/tor-expert-bundle/pt_config.json?ref_type=heads
 mv "pt_config.json?ref_type=heads" pt_config.json
-rm orbotservice/src/main/assets/snowflake-brokers
+rm -f orbotservice/src/main/assets/snowflake-brokers
 function bridges_conf {
   local bridge_type="$1"
   jq -r ".bridges.\"$bridge_type\" | .[]" "pt_config.json" | while read -r line; do
