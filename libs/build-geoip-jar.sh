ba8a25400a145f7adf0e9bd4236f7718ee42bbb3
a <dsnake@protonmail.com>
2024-05-25 01:38:00 -0400

    made geoip script freshly clone the repo, actually updated geoip files

--- libs/build-geoip-jar.sh
+++ libs/build-geoip-jar.sh
@@ -1,7 +1,8 @@
-# assumes tor is in a directory next to orbot 
 rm geoip.jar
 mkdir assets
-cp -a ../../tor/src/config/geoip assets/
-cp -a ../../tor/src/config/geoip6 assets/
+git clone https://gitlab.torproject.org/tpo/core/tor.git --depth=1
+cp -a tor/src/config/geoip assets/
+cp -a tor/src/config/geoip6 assets/
+rm -rf tor
 zip -o geoip.jar assets/geoip assets/geoip6
 rm -rf assets
