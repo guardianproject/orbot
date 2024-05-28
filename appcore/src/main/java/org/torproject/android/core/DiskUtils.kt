b6feefe11934a519e510185831fe9d5f376d74b2
a <dsnake@protonmail.com>
2024-05-28 12:28:24 -0400

    deals with easy to fix kotlin warnings

--- appcore/src/main/java/org/torproject/android/core/DiskUtils.kt
+++ appcore/src/main/java/org/torproject/android/core/DiskUtils.kt
@@ -26,11 +26,12 @@ object DiskUtils {
     }
 
     @JvmStatic
-    fun createWriteFileIntent(filename: String, mimeType: String): Intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-        addCategory(Intent.CATEGORY_OPENABLE)
-        type = mimeType
-        putExtra(Intent.EXTRA_TITLE, filename)
-    }
+    fun createWriteFileIntent(filename: String, mimeType: String): Intent =
+        Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = mimeType
+            putExtra(Intent.EXTRA_TITLE, filename)
+        }
 
     @JvmStatic
     fun createReadFileIntent(mimeType: String): Intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
@@ -40,17 +41,9 @@ object DiskUtils {
 
     @JvmStatic
     fun readFileFromInputStream(contentResolver: ContentResolver, file: Uri): String {
-        val text = StringBuilder()
         val input = contentResolver.openInputStream(file)
         val reader = BufferedReader(input!!.reader())
-        reader.use { reader ->
-            var line = reader.readLine()
-            while (line != null) {
-                text.append(line)
-                line = reader.readLine()
-            }
-        }
-        return text.toString()
+        return reader.use(BufferedReader::readText)
     }
 
     @JvmStatic
